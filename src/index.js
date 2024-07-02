40fc948ede2b26e98315b3a5aaf44d12f5d79d89
deep-vinci <deepakworkpc@gmail.com>
2024-07-02 13:23:07 +0530

    adding input listener

--- src/index.js
+++ src/index.js
@@ -1,39 +1,7 @@
 import "./style.css"
 
-{/* <div class="repository" id="repo-1">
-<div class="info">
-    <div class="svg-div">
-        <svg fill="rgb(141, 150, 160)" aria-hidden="true" height="18" viewBox="0 0 18 18" version="1.1"
-            width="18" data-view-component="true" class="octicon octicon-repo mr-1 color-fg-muted">
-            <path
-                d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z">
-            </path>
-        </svg>
-    </div>
-    <div class="repository-name"><a href="#">tictactoe</a></div>
-    <div class="public">Public</div>
-</div>
-
-<p class="info-2">
-    <span class="language-stats">
-        <span class="language-color" style="background-color: #f1e05a"></span>
-        <span itemprop="language">JavaScript</span>
-    </span>
-
-    <a href="/deep-vinci/tictactoe/stargazers" class="stargazers">
-        <svg fill="rgb(141, 150, 160)" aria-label="stars" role="img" height="18" viewBox="0 0 15 15" version="1.1" width="18"
-            data-view-component="true" class="octicon octicon-star">
-            <path
-                d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z">
-            </path>
-        </svg>
-        3
-    </a>
-</p>
-</div> */}
-
 const repositoryContainer = document.querySelector(".repositories");
-
+const searchInput = document.querySelector("#search")
 
 const createRepoDiv = (repository) => {
     // console.log(repository)
@@ -124,6 +92,18 @@ const createRepoDiv = (repository) => {
     languageStatsSpan.appendChild(languageSpan);
     info2Div.appendChild(starGazers);
 
+    let repoDivs = document.querySelectorAll(".repository");
+
+    searchInput.addEventListener('input', (e) => {
+        // console.log(e.target.value)
+        repoDivs.forEach(elem => {
+            elem.remove();
+        })
+        // console.log(repoDivs)
+
+        printRepoData(searchRepoData(e.target.value))
+    });
+
 }
 
 const getRepoData = async () => {
@@ -135,14 +115,15 @@ const getRepoData = async () => {
     return data
 }
 
-const printRepoData = async () => {
+const printRepoData = async (repoObj) => {
     let data = await getRepoData(); // one liner before importing the data to any function
 
     // console.log(data[0])
-    
-    data.forEach(repository => {
-        // console.log(repository)
-        console.log(`${repository.language} ${repository.stargazers_count} ${repository.name} ${repository.description == null ? "" : "/ " + repository.description}`)
+    let dataResp = repoObj === undefined ? data : repoObj;
+    console.log(await dataResp)
+    dataResp.forEach(repository => {
+
+        // console.log(`${repository.language} ${repository.stargazers_count} ${repository.name} ${repository.description == null ? "" : "/ " + repository.description}`)
         createRepoDiv(repository)
 
     });
@@ -153,12 +134,14 @@ let searchRepoData = async (searchTerm) => {
     let searchTermRegex = new RegExp(`^${searchTerm}`, 'i'); // Creating RegExp dynamically
     const searchedRepoOutput = repoData.filter(repo => searchTermRegex.test(repo.name))
     
-    console.log(searchedRepoOutput)
+    // console.log(searchedRepoOutput)
+    return searchedRepoOutput
     // repoData.forEach(repo => {
     //     console.log(`${searchTermRegex.test(repo.name) == true ? 1 : 0} ${repo.name}`)
     // })
 }
 
 
-searchRepoData("tic")
+// searchRepoData("s")
 printRepoData()
+
