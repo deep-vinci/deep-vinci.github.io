17ae9b0a3ac8bf398b02e214ac7cd4a14dac083a
deep-vinci <deepakworkpc@gmail.com>
2024-07-03 11:03:41 +0530

    adding user search

--- src/index.js
+++ src/index.js
@@ -11,21 +11,21 @@ const createRepoDiv = (repository) => {
         css: "#563d7c",
         markdown: "#083fa1",
     }
-    
-    
+
+
     const SVG = {
         repositoryIcon: `<svg fill="rgb(141, 150, 160)" aria-hidden="true" height="18" viewBox="0 0 18 18" version="1.1"
-                            width="18" data-view-component="true" class="octicon octicon-repo mr-1 color-fg-muted">
-                            <path
-                                d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z">
-                            </path>
-                        </svg>`,
+width="18" data-view-component="true" class="octicon octicon-repo mr-1 color-fg-muted">
+<path
+d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z">
+</path>
+</svg>`,
         starGazersIcon: `<svg fill="rgb(141, 150, 160)" aria-label="stars" role="img" height="18" viewBox="0 0 15 15" version="1.1" width="18"
-                            data-view-component="true" class="octicon octicon-star">
-                            <path
-                                d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z">
-                            </path>
-                        </svg>`
+data-view-component="true" class="octicon octicon-star">
+<path
+d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z">
+</path>
+</svg>`
     }
 
     const repoName = repository.name;
@@ -69,9 +69,9 @@ const createRepoDiv = (repository) => {
     publicDiv.textContent = publicText;
 
     languageSpan.textContent = language == null ? "no lang" : language;
-    languageColorSpan.style.backgroundColor = languageColors[language === null ? null : language.toLowerCase() ] == null ? "gray" : languageColors[language === null ? null : language.toLowerCase() ];
+    languageColorSpan.style.backgroundColor = languageColors[language === null ? null : language.toLowerCase()] == null ? "gray" : languageColors[language === null ? null : language.toLowerCase()];
     starGazers.innerHTML = SVG.starGazersIcon + starGazersCount;
-    
+
     // appending the nodes
 
     repositoryContainer.appendChild(repositoryDiv);
@@ -92,15 +92,20 @@ const createRepoDiv = (repository) => {
 
 const getRepoData = async () => {
     // in future this could be removed to bring the github actions
-
-    let response = await fetch("https://api.github.com/users/deep-vinci/repos");
+    // Experience-Monks/repositories
+    // let response = await fetch("https://api.github.com/users/deep-vinci/repos");
+    let response = await fetch("https://api.github.com/users/Experience-Monks/repos");
     let data = await response.json()
 
     return data
 }
 
+
+let repoData = await getRepoData();
+
+
 const printRepoData = async (repoObj) => {
-    let data = await getRepoData(); // one liner before importing the data to any function
+    let data = repoData; // one liner before importing the data to any function
 
     let dataResp = await repoObj === undefined ? data : repoObj;
 
@@ -109,11 +114,11 @@ const printRepoData = async (repoObj) => {
     });
 }
 
-let searchRepoData = async (searchTerm) => {
-    let repoData = await getRepoData();
-    let searchTermRegex = new RegExp(`^${searchTerm}`, 'i'); // Creating RegExp dynamically
+let searchRepoData = async (searchTerm, repoData) => {
+    // let repoData = await getRepoData();
+    let searchTermRegex = new RegExp(`${searchTerm}`, 'i'); // Creating RegExp dynamically
     const searchedRepoOutput = repoData.filter(repo => searchTermRegex.test(repo.name))
-    
+
     return searchedRepoOutput
 }
 
@@ -128,5 +133,14 @@ searchInput.addEventListener('input', async (e) => {
         elem.remove();
     })
 
-    printRepoData(await searchRepoData(e.target.value))
+    printRepoData(await searchRepoData(e.target.value, repoData))
 });
+
+let seeYourAcc = document.querySelector(".see-your-acc");
+let userSearch = document.querySelector(".user-search");
+
+seeYourAcc.addEventListener("click", () => {
+    if(userSearch.value.trim() !== "") {
+        console.log("filled")
+    }
+})
