8aaf49567f0d32e84c71d6460bce47a532194b58
Deepak <deepakworkpc@gmail.com>
2025-05-31 11:45:06 +0530

    feat: implemented 100 repo per page rather than default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "./style.css"
+import "./style.css";
 
-import { createRepoDiv } from "./component/createRepoDiv.js"
+import { createRepoDiv } from "./component/createRepoDiv.js";
 
 const me = "deep-vinci";
 
@@ -13,84 +13,86 @@ let userImage = document.querySelector(".user-image");
 
 const getRepoData = async (user) => {
     if (user == me) {
-        let response = await fetch(`https://raw.githubusercontent.com/deep-vinci/deep-vinci.github.io/refs/heads/main/prefetch-data.json`);
-        let data = await response.json()
+        let response = await fetch(
+            `https://raw.githubusercontent.com/deep-vinci/deep-vinci.github.io/refs/heads/main/prefetch-data.json`
+        );
+        let data = await response.json();
         if (data.status == 404) {
-            console.log('error')
+            console.log("error");
         }
         globalRepoData = data;
-        return globalRepoData
+        return globalRepoData;
     } else {
-        let response = await fetch(`https://api.github.com/users/${user}/repos`);
+        let response = await fetch(
+            `https://api.github.com/users/${user}/repos?per_page=100`
+        );
         // let response = await fetch("https://api.github.com/users/Experience-Monks/repos");
-        let data = await response.json()
+        let data = await response.json();
         // console.log(globalRepoData)
         // if (data.message == undefined) {
         //     alert("rate limited")
         // }
         if (data.status == 404) {
-            console.log('error')
+            console.log("error");
         }
         globalRepoData = data;
-        return globalRepoData    
+        return globalRepoData;
     }
-
-}
+};
 
 const clearRepositoriesContainer = () => {
     let repoDivs = document.querySelectorAll(".repository");
 
-    repoDivs.forEach(elem => {
+    repoDivs.forEach((elem) => {
         elem.remove();
-    })
-
-}
+    });
+};
 
 const printRepoData = async (data) => {
     clearRepositoriesContainer();
     if (globalRepoData.status == 404) {
-        
         return;
-    };
+    }
 
-    console.log(globalRepoData)
-    console.log(data[0].owner.avatar_url)
+    console.log(globalRepoData);
+    console.log(data[0].owner.avatar_url);
     // userImage.style.background = data[0].owner.avatar_url;
     // userImage.style.background = "blue"
     userImage.style.backgroundImage = `url(${data[0].owner.avatar_url})`;
-    data.forEach(repository => {
-        createRepoDiv(repository)
+    data.forEach((repository) => {
+        createRepoDiv(repository);
     });
-}
+};
 
 userImage.addEventListener("click", () => {
     // console.log(data[0])
 
-    window.open(`https://github.com/${globalRepoData[0].owner.login}`)
-})
+    window.open(`https://github.com/${globalRepoData[0].owner.login}`);
+});
 
 let searchRepoData = async (searchTerm, globalRepoData) => {
     if (globalRepoData.status == 404) return;
 
-    let searchTermRegex = new RegExp(`${searchTerm}`, 'i'); // Creating RegExp dynamically
-    const searchedRepoOutput = globalRepoData.filter(repo => searchTermRegex.test(repo.name))
+    let searchTermRegex = new RegExp(`${searchTerm}`, "i"); // Creating RegExp dynamically
+    const searchedRepoOutput = globalRepoData.filter((repo) =>
+        searchTermRegex.test(repo.name)
+    );
 
-    return searchedRepoOutput
-}
+    return searchedRepoOutput;
+};
 
 seeYourAcc.addEventListener("click", async () => {
     if (userSearch.value.trim() !== "") {
         await getRepoData(userSearch.value);
-        printRepoData(await searchRepoData("", globalRepoData))
+        printRepoData(await searchRepoData("", globalRepoData));
     }
-})
+});
 
-searchInput.addEventListener('input', async (e) => {
-    printRepoData(await searchRepoData(e.target.value, globalRepoData))
+searchInput.addEventListener("input", async (e) => {
+    printRepoData(await searchRepoData(e.target.value, globalRepoData));
 });
 
 document.addEventListener("readystatechange", async () => {
     await getRepoData(me);
-    printRepoData(await searchRepoData("", globalRepoData))
+    printRepoData(await searchRepoData("", globalRepoData));
 });
-
